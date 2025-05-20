20bfbc5a852854ad9559d9a82aa3b1c86d92bba8
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-05-20 03:10:30 +0200

    origins for dev

--- src/main/java/com/alex/projectrapi/config/SecurityConfig.java
+++ src/main/java/com/alex/projectrapi/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(Arrays.asList("http://localhost:8100"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:8100", "http://192.168.27.27:8100"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         config.setExposedHeaders(Arrays.asList("Authorization")); // Asegura que el frontend pueda leer el header

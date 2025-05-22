7fc49c292333839526c139e54ac54bc20ffe65c3
Alphatule <66392548+alphatule@users.noreply.github.com>
2025-05-22 17:21:01 +0200

    remove: websocket mierdon

--- src/main/java/com/alex/projectrapi/dto/SocketMessageRequest.java
+++
@@ -1,8 +0,0 @@
-package com.alex.projectrapi.dto;
-
-public class SocketMessageRequest {
-    private String content;
-
-    public String getContent() { return content; }
-    public void setContent(String content) { this.content = content; }
-}
